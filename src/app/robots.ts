c3662497c74bc84ea6f86f15b37c42ec6de4cb6d
Johny-88 <cebotari.77.ion@gmail.com>
2026-08-21 16:27:20 +0100

    Allow search crawling and publish sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,10 +1,18 @@
 import type { MetadataRoute } from "next";
+import { SITE_URL } from "../lib/site";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      disallow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "OAI-SearchBot",
+        allow: "/",
+      },
+    ],
+    sitemap: `${SITE_URL}/sitemap.xml`,
   };
 }
