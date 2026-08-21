3d9131e904baccde7d66c9992fcdc73fe3abdb01
Johny-88 <cebotari.77.ion@gmail.com>
2026-08-21 16:27:13 +0100

    Restore indexable site metadata defaults

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,19 +10,6 @@ import "./service-home.css";
 export const metadata: Metadata = {
   metadataBase: new URL(SITE_URL),
   applicationName: SITE_NAME,
-  robots: {
-    index: false,
-    follow: false,
-    nocache: true,
-    googleBot: {
-      index: false,
-      follow: false,
-      noimageindex: true,
-      "max-image-preview": "none",
-      "max-snippet": 0,
-      "max-video-preview": 0,
-    },
-  },
   icons: {
     icon: [{ url: "/assets/favicon.svg", type: "image/svg+xml" }],
   },
