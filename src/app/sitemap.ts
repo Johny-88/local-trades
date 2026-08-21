ca9b7a131a99a9131291323b9e48170aa19abae6
Johny-88 <cebotari.77.ion@gmail.com>
2026-08-21 13:43:18 +0100

    Disable sitemap URL discovery while site is private

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,29 +1,5 @@
 import type { MetadataRoute } from "next";
-import { PUBLISHED_LOCAL_PAGES, localPagePath } from "../lib/localPages";
-import { SITE_URL } from "../lib/site";
-
-const STATIC_PATHS = [
-  "/",
-  "/about/",
-  "/handyman/",
-  "/plumber/",
-  "/electrician/",
-  "/roofer/",
-  "/painter-decorator/",
-  "/flooring-fitter/",
-  "/builder/",
-  "/gardener/",
-  "/contact/",
-  "/privacy/",
-  "/cookies/",
-  "/terms/",
-] as const;
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    ...STATIC_PATHS.map((path) => ({ url: `${SITE_URL}${path}` })),
-    ...PUBLISHED_LOCAL_PAGES.map((page) => ({
-      url: `${SITE_URL}${localPagePath(page.trade, page.city)}`,
-    })),
-  ];
+  return [];
 }
