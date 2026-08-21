99d240cac0b8f3c0f788aa8f623f8edafd3008c2
Johny-88 <cebotari.77.ion@gmail.com>
2026-08-21 21:39:15 +0100

    Add Local Trades contact details

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -4,3 +4,5 @@ export const SITE_LOCALE = "en_GB";
 export const SITE_LANGUAGE = "en-GB";
 export const THEME_COLOR = "#faf6f0";
 export const DEFAULT_OG_IMAGE = "/og/home.jpg";
+export const CONTACT_EMAIL = "getlocaltrades@gmail.com";
+export const OPERATOR_NAME = "Ion Cebotari";
