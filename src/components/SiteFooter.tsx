004204b97a2ff93b5b3758f4a4ff72f7f885b8b4
Johny-88 <cebotari.77.ion@gmail.com>
2026-08-21 21:07:49 +0100

    Remove Birmingham service link from global footer

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -16,7 +16,6 @@ export function SiteFooter() {
           <a href="/">Home</a>
           <a href="/about/">About</a>
           <a href="/contact/">Contact</a>
-          <a href="/removal-companies/birmingham/">Removal companies Birmingham</a>
         </div>
 
         <div className="footer-links" aria-label="Legal">
