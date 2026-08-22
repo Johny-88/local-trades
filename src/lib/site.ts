16206d9729e2b7166c8d34ed55b487051afbb261
Johny-88 <cebotari.77.ion@gmail.com>
2026-08-22 20:30:48 +0100

    Use PNG homepage OG image

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -3,6 +3,6 @@ export const SITE_URL = "https://getlocaltrades.uk";
 export const SITE_LOCALE = "en_GB";
 export const SITE_LANGUAGE = "en-GB";
 export const THEME_COLOR = "#faf6f0";
-export const DEFAULT_OG_IMAGE = "/og/home.jpg";
+export const DEFAULT_OG_IMAGE = "/og/home.png";
 export const CONTACT_EMAIL = "getlocaltrades@gmail.com";
 export const OPERATOR_NAME = "Ion Cebotari";
